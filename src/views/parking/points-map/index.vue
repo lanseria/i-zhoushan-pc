47157d07da6f218db97f12b09d5f4387c957a5e5
lanseria <zhangchao564265135@hotmail.com>
2022-12-05 14:27:26 +0800

    update

--- src/views/parking/points-map/index.vue
+++ src/views/parking/points-map/index.vue
@@ -1,61 +1,29 @@
 <script lang="ts" setup>
 import mapboxgl from 'mapbox-gl'
 import MapboxLanguage from '@mapbox/mapbox-gl-language'
-import { ControlEvent, DrawControl, DrawEvent, DrawPoint } from '@antv/l7-draw'
+import { DrawEvent, DrawPoint } from '@antv/l7-draw'
 import { Scene } from '@antv/l7'
 import { Mapbox } from '@antv/l7-maps'
 let mapRef: mapboxgl.Map | null = null
 let sceneRef: Scene | null = null
-let drawControlRef: DrawControl | null = null
+let drawerRef: DrawPoint | null = null
 
-const visible = ref(false)
-
-const handleShowStatus = () => {
-  if (drawControlRef)
-    drawControlRef.hide()
+const handleSave = () => {
+  drawerRef?.disable()
 }
 
-const handleEditStatus = () => {
-  if (drawControlRef) {
-    drawControlRef.show()
-  }
-  else {
-    // 实例化 DrawControl 实例
-    const drawControl = new DrawControl(sceneRef!, {
-    // DrawControl 参数
-      position: 'topcenter',
-      commonDrawOptions: {
-        editable: true,
-        multiple: false,
-      },
-    })
-    drawControlRef = drawControl
-    // 将 Control 添加至地图中
-    sceneRef!.addControl(drawControl)
-    drawControlRef.on(ControlEvent.DrawChange, (newType) => {
-      console.log('当前激活的绘制发生更改', newType)
-    })
-
-    drawControlRef.on(ControlEvent.DataChange, (newData) => {
-      console.log('当前绘制数据发生更改', newData)
-      visible.value = true
-      // drawControlRef!.setActiveType(null)
-    })
-  }
+const handleRefresh = () => {
+  drawerRef?.disable()
 }
 
-const handleOk = () => {
-  visible.value = false
-}
-
-const handleCancel = () => {
-  visible.value = false
+const handleAddPoint = () => {
+  drawerRef?.enable()
 }
 
 onMounted(async () => {
   mapboxgl.accessToken = 'pk.eyJ1IjoibGFuc2VyaWEiLCJhIjoiY2tzNDE4MDI0MHg5ZjJvcndtZzF4YTB6aCJ9.5k-y1Bx3km5MAOp4KVpb9g'
   mapRef = new mapboxgl.Map({
-    container: 'map', // container ID
+    container: 'map', // container-ID
     style: 'mapbox://styles/lanseria/cl2ipj7ar004z14nv9lgrdb2d', // style URL
     center: [122.16327998508143, 30.002517329286277], // starting position [lng, lat]
     zoom: 9, // starting zoom
@@ -88,6 +56,19 @@ onMounted(async () => {
     // setInterval(() => {
     //   console.log(scene!.getZoom(), scene!.getCenter())
     // }, 2000)
+
+    //
+    drawerRef = new DrawPoint(sceneRef!, {
+      autoActive: false,
+      // Draw 的配置 options
+    })
+    // 开启绘制
+    drawerRef.disable()
+
+    // 监听绘制事件
+    drawerRef.on(DrawEvent.Change, (pointList) => {
+      console.log(pointList)
+    })
   })
 })
 onUnmounted(() => {
@@ -97,34 +78,20 @@ onUnmounted(() => {
 
 <template>
   <LayoutContainer>
+    <ASpace class="mb-5">
+      <a-button type="outline" @click="handleSave()">
+        保存
+      </a-button>
+      <a-button type="outline" @click="handleRefresh()">
+        刷新
+      </a-button>
+      <a-button type="outline" @click="handleAddPoint()">
+        添加点
+      </a-button>
+    </ASpace>
     <div
       id="map"
       class="relative h-[calc(100vh-300px)]"
-    >
-      <ASpace class="absolute top-10px left-10px z-100">
-        <a-button type="outline" @click="handleShowStatus()">
-          查看地图
-        </a-button>
-        <a-button type="outline" @click="handleEditStatus()">
-          编辑地图
-        </a-button>
-      </ASpace>
-      <ADrawer
-        popup-container="#map"
-        :height="200"
-        :mask="false"
-        :mask-closable="false"
-        :visible="visible"
-        placement="bottom"
-        unmount-on-close
-        @ok="handleOk"
-        @cancel="handleCancel"
-      >
-        <template #title>
-          Title
-        </template>
-        <div>You can customize modal body text by the current situation. This modal will be closed immediately once you press the OK button.</div>
-      </ADrawer>
-    </div>
+    />
   </LayoutContainer>
 </template>
